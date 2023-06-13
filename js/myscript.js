9bed9f11a293054f5c6ca4975858edde208c496f
CuonoMaione <cuonomaione99@outlook.it>
2023-06-13 17:25:04 +0200

    fix getemail

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,18 @@
 
 
+
 function GetEmail() {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then(function(response){
-    console.log(response.data.response)
-});
-
+    .then(function(response){
+      return response.data.response ;
+    }) 
 }
+
+
+;
+
 console.log(GetEmail())
+
 
 
 
@@ -15,8 +20,11 @@ let email = [];
 
 for (let i = 0; i < 10; i++) {
     
-   email.push(GetEmail());
+   
     
+   email.push(GetEmail());
+   
+
 }
 
 console.log(email)
